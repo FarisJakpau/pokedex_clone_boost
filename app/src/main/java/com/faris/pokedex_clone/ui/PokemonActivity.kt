1a7a8005ec3189256ad9a5edc9159e51050f1f3f
farisjakpau <farisjaafar418@gmail.com>
2021-03-06 16:20:47 +0800

    add pokemon activty

---
+++ app/src/main/java/com/faris/pokedex_clone/ui/PokemonActivity.kt
@@ -0,0 +1,19 @@
+package com.faris.pokedex_clone.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.faris.pokedex_clone.R
+
+/**
+ * Created by farisjakpau on 06/03/2021.
+ * Pokedex_Clone.
+ */
+
+class PokemonActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemon)
+
+    }
+}
